293ad0e0bde8c3f6205d58c3224dcc088cb35065
FacuRodriJ <facurodri00@gmail.com>
2022-09-22 10:13:06 -0300

    Estilos corregidos base.html, login.html, profile.html, register.html.

--- accounts/models.py
+++ accounts/models.py
@@ -8,20 +8,21 @@ from django.conf import settings
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, verbose_name='Email')
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     """Atributo OneToOneField para relacionar el modelo Profile con el modelo User."""
 
-    bio = models.TextField(max_length=500, null=True, blank=True)
+    bio = models.TextField(max_length=500, null=True, blank=True, verbose_name='Biografía')
     """Atributo para almacenar la biografía del usuario."""
 
     def avatar_directory_path(self, filename):
         """Funcion para guardar el archivo en MEDIA_ROOT/user_<id>/<filename>"""
         return 'profile_avatars/user_{0}/{1}'.format(self.user.id, filename)
 
-    avatar = models.ImageField(upload_to=avatar_directory_path, null=True, blank=True)
+    avatar = models.ImageField(upload_to=avatar_directory_path, null=True, blank=True, verbose_name='Avatar')
     """Atributo para almacenar la imagen de perfil del usuario."""
 
     def __str__(self):
