e261069aa7dbff9600b22588b1c97808acbd3674
FacuRodriJ <facurodri00@gmail.com>
2022-09-22 15:17:33 -0300

    App accounts models.py y admin.py actualizados.

--- accounts/models.py
+++ accounts/models.py
@@ -4,25 +4,26 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from PIL import Image
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(unique=True, verbose_name='Email')
+    email = models.EmailField(unique=True, verbose_name=_('Email'))
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     """Atributo OneToOneField para relacionar el modelo Profile con el modelo User."""
 
-    bio = models.TextField(max_length=500, null=True, blank=True, verbose_name='Biografía')
+    bio = models.TextField(max_length=500, null=True, blank=True, verbose_name=_('Biografía'))
     """Atributo para almacenar la biografía del usuario."""
 
     def avatar_directory_path(self, filename):
         """Funcion para guardar el archivo en MEDIA_ROOT/user_<id>/<filename>"""
         return 'profile_avatars/user_{0}/{1}'.format(self.user.id, filename)
 
-    avatar = models.ImageField(upload_to=avatar_directory_path, null=True, blank=True, verbose_name='Avatar')
+    avatar = models.ImageField(upload_to=avatar_directory_path, null=True, blank=True, verbose_name=_('Avatar'))
     """Atributo para almacenar la imagen de perfil del usuario."""
 
     def __str__(self):
@@ -48,3 +49,7 @@ class Profile(models.Model):
             return self.avatar.url
         else:
             return settings.STATIC_URL + 'img/empty.png'
+
+    class Meta:
+        verbose_name = _('Perfil')
+        verbose_name_plural = _('Perfiles')
