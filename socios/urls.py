13d3782b24313c6fc4b284b2f697688b9739807a
FacuRodriJ <facurodri00@gmail.com>
2022-11-20 00:00:37 -0300

    Urls, Dashboard y links activos actualizados en la app Socios

--- socios/urls.py
+++ socios/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.shortcuts import redirect
 
 from socios.views.admin.socio.views import *
 from socios.views.socio.views import SocioFormView
@@ -6,7 +7,17 @@ from socios.views.solicitud.views import *
 
 urlpatterns = [
     # Socios, URLs de administración
-    path('admin/socios/', SocioAdminListView.as_view(), name='admin-socio-listado'),
+
+    # Si ingresan a admin/socios/ se redirige a admin-socio-listado
+    path('admin/socios/', lambda request: redirect('admin-socio-listado', permanent=True), name='admin-socio'),
+    path('admin/socios/listado/', SocioAdminListView.as_view(), name='admin-socio-listado'),
+    path('admin/socios/solicitudes/', SolicitudAdminListView.as_view(), name='admin-socio-solicitudes'),
+    # TODO: Agregar la vista del listado de cuotas sociales
+    path('admin/socios/cuotas/', lambda request: redirect('admin-socio-listado', permanent=True),
+         name='admin-socio-cuotas'),
+    # TODO: Agregar la vista de parámetros socios
+    path('admin/socios/parametros/', lambda request: redirect('admin-socio-listado', permanent=True),
+         name='admin-socio-parametros'),
     path('admin/socios/<int:pk>/', SocioAdminDetailView.as_view(), name='admin-socio-detalle'),
     path('admin/socios/<int:pk>/editar/', SocioAdminUpdateView.as_view(), name='admin-socio-editar'),
     path('admin/socios/<int:pk>/eliminar/', socio_delete, name='admin-socio-eliminar'),
@@ -15,8 +26,7 @@ urlpatterns = [
     # Socios, URLs de usuario
     path('socio/', SocioFormView.as_view(), name='socio'),
 
-    # Solicitud de asociación, URLs de administración
-    path('admin/solicitud_socios/', SolicitudAdminListView.as_view(), name='admin-solicitud-listado'),
+
 
     # Solicitud de asociación, URLs de usuario
     path('solicitud/', SolicitudView.as_view(), name='solicitud-crear'),
